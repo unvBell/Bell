5b02aa6338b6dd28dba8936a482c15089529dd4f
unvBell <ryoga_github-2048@yahoo.co.jp>
2015-08-03 23:26:25 +0900

    数学定数πの追加

---
+++ src/bell/math/Constants.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+namespace bell { namespace math {
+	/**
+	 * 円周率
+	 * @return π
+	 */
+	template <typename T>
+	inline constexpr T pi() noexcept {
+		return static_cast<T>(3.1415926535897932384626433832795028841971693993L);
+	}
+}}
---
+++ src/bell/math/Test_Constants.cpp
@@ -0,0 +1,14 @@
+﻿#ifdef _DEBUG
+
+#include <gtest/gtest.h>
+#include "Constants.hpp"
+
+using namespace bell::math;
+
+TEST(Bell_Math_Constants, PI) {
+	EXPECT_EQ(3.14159265358979323846L, pi<long double>());
+	EXPECT_EQ(3.14159265358979323846 , pi<double>()		);
+	EXPECT_EQ(3.14159265358979323846f, pi<float>()		);
+}
+
+#endif
