1ca547f93df6c6fc869bb5297bb1d45dc1ac34e2
unvBell <ryoga_github-2048@yahoo.co.jp>
2015-08-05 20:41:30 +0900

    色構造体の追加

---
+++ src/bell/graphics/Color.hpp
@@ -0,0 +1,69 @@
+﻿#pragma once
+
+#include <cstdint>
+
+namespace bell { namespace graphics {
+	/**
+	 * 色
+	 */
+	struct Color final {
+		float r, g, b, a;
+
+		//	コンストラクタ
+		constexpr Color() noexcept
+			: r(0), g(0), b(0), a(1.f) {}
+
+		constexpr Color(float r, float g, float b, float a = 1.f) noexcept
+			: r(r), g(g), b(b), a(a) {}
+
+		constexpr Color(uint32_t argb) noexcept
+			: r(((argb >> 16) & 0xff) / 255.f)
+			, g(((argb >>  8) & 0xff) / 255.f)
+			, b(((argb >>  0) & 0xff) / 255.f)
+			, a(((argb >> 24) & 0xff) / 255.f) {}
+
+
+		/**
+		 * RGBA値から色の算出
+		 * @param  r [0,1]
+		 * @param  g [0,1]
+		 * @param  b [0,1]
+		 * @param  a [0,1] (default:1.f)
+		 * @return   色
+		 */
+		constexpr static Color rgb(float r, float g, float b, float a = 1.f) noexcept;
+
+
+		/**
+		 * HSVA値から色の算出
+		 * @param  h 色相[0,2π]
+		 * @param  s 彩度[0,1]
+		 * @param  v 明度[0,1]
+		 * @param  a アルファ[0,1] (default:1.f)
+		 * @return   色
+		 */
+		/*constexpr*/ static Color hsv(float h, float s, float v, float a = 1.f) noexcept;
+
+
+		/**
+		 * 2つの色の等値比較
+		 * @param  c 比較対象の色
+		 * @return   全ての要素が等しければtrue
+		 */
+		constexpr bool equals(const Color& c) const noexcept {
+			return r == c.r && g == c.g && b == c.b && a == c.a;
+		}
+
+
+		//	演算子
+		constexpr bool operator==(const Color& c) const noexcept {
+			return equals(c);
+		}
+
+		constexpr bool operator!=(const Color& c) const noexcept {
+			return !equals(c);
+		}
+	};
+}}
+
+#include "Color.inl"
---
+++ src/bell/graphics/Color.inl
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "../Assert.hpp"
+#include "../math/Constants.hpp"
+#include "../math/Floor.hpp"
+
+namespace bell { namespace graphics {
+	//	RGBA
+	inline constexpr Color Color::rgb(float r, float g, float b, float a) noexcept {
+		return
+			BELL_ASSERT(0 <= r && r <= 1.f),
+			BELL_ASSERT(0 <= g && g <= 1.f),
+			BELL_ASSERT(0 <= b && b <= 1.f),
+			BELL_ASSERT(0 <= a && a <= 1.f),
+			Color{ r, g, b, a };
+	}
+
+
+	//	HSVA
+	inline /*constexpr*/ Color Color::hsv(float h, float s, float v, float a) noexcept {
+		BELL_ASSERT(0 <= s && s <= 1.f);
+		BELL_ASSERT(0 <= v && v <= 1.f);
+		BELL_ASSERT(0 <= a && a <= 1.f);
+		
+		h /= 2*pi<float>() / 6;
+
+		int   i = static_cast<int>(floor(h));
+		float f = h - i;
+		float p = v * (1 - s);
+		float q = v * (1 - s*f);
+		float r = v * (1 - s*(1-f));
+
+		switch((i%6+6)%6) {
+		case 0: return { v, r, p, a };
+		case 1: return { q, v, p, a };
+		case 2: return { p, v, r, a };
+		case 3: return { p, q, v, a };
+		case 4: return { r, p, v, a };
+		case 5: return { v, p, q, a };
+		}
+
+		return BELL_ASSERT(false), Color{};
+	}
+}}
