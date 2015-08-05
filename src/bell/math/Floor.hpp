e11503d04323e756bcf6d3d4939325ec81bebd3f
unvBell <ryoga_github-2048@yahoo.co.jp>
2015-08-05 20:41:14 +0900

    floor()の追加

---
+++ src/bell/math/Floor.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <type_traits>
+
+namespace bell { namespace math {
+	/**
+	 * 床関数
+	 * @param  x
+	 * @return   [x]
+	 */
+	template <typename Float>
+	constexpr Float floor(Float x) noexcept {
+		static_assert(
+			std::is_floating_point<Float>::value,
+			"bell::math::floor<Float> Float must be floating point.");
+
+		return
+			static_cast<long>(x) == x
+				? x
+				: x < 0
+					? static_cast<long>(x)-1
+					: static_cast<long>(x);
+	}
+}}
+
+namespace bell {
+	using bell::math::floor;
+}
---
+++ src/bell/math/Test_Floor.cpp
@@ -0,0 +1,25 @@
+﻿#ifdef _DEBUG
+
+//#include <gtest/gtest.h>
+#include "Floor.hpp"
+
+using bell::math::floor;
+
+//TEST(Bell_Math_Floor, Floor) {
+	static_assert(floor( 5.0f) ==  5.f, "");
+	static_assert(floor(-5.0f) == -5.f, "");
+	static_assert(floor( 2.5f) ==  2.f, "");
+	static_assert(floor(-2.5f) == -3.f, "");
+	
+	static_assert(floor( 5.0) ==  5., "");
+	static_assert(floor(-5.0) == -5., "");
+	static_assert(floor( 2.5) ==  2., "");
+	static_assert(floor(-2.5) == -3., "");
+	
+	static_assert(floor( 5.0L) ==  5.L, "");
+	static_assert(floor(-5.0L) == -5.L, "");
+	static_assert(floor( 2.5L) ==  2.L, "");
+	static_assert(floor(-2.5L) == -3.L, "");
+//}
+
+#endif
